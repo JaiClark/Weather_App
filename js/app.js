b214f6bdfc5751465fba253d7d99405c6abfa53a
Jaurs <rhsemail01@gmail.com>
2018-11-20 03:34:02 -0500

    Progress as of 11/20/2018

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-var x = document.getElementById("denied");
-var lat = position.coords.latitude;
-var long = position.coords.longitude;
+var x = document.getElementById("success");
 
+
+
+// Functiont that will get the location from the user
 function getLocation() {
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
@@ -10,25 +12,33 @@ function getLocation() {
     }
 }
 
-function showPosition(position) {
-    console.log("Latitude: " + position.coords.latitude + 
-    " Longitude: " + position.coords.longitude);
-    console.log(lat);
-    console.log(long);
-}
 
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            console.log( "User denied the request for Geolocation.");
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
+
+
+// FUnction that shows the position of the user
+function showPosition(position) {
+
+
+var lat = position.coords.latitude;
+var long = position.coords.longitude;
+
+
+    x.innerHTML = "Latitude: " + lat + 
+    " Longitude: " + long;
+
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function(){
+        if(http.readyState === 4 && http.status === 200){
+            console.log(http.response);
+        }
     }
+
+   
+    http.open('GET', "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon="+ long, true);
+    http.send();
+
+
 }
+    
+
