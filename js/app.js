d60634498f3faa1b994ebd2d076cc1b013360826
Jaurs <rhsemail01@gmail.com>
2018-11-26 20:47:43 -0500

    Progress as of 11/26

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 var x = document.getElementById("success");
+var tempUnit = "&#8451";
+var newTempUnit = "&#8457";
 
-
-
-// Functiont that will get the location from the user
+// Function that will get the location from the user
 function getLocation() {
-
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else { 
@@ -12,33 +11,51 @@ function getLocation() {
     }
 }
 
-
-
-
-// FUnction that shows the position of the user
+// Function that shows the position of the user
 function showPosition(position) {
-
 
 var lat = position.coords.latitude;
 var long = position.coords.longitude;
-
-
-    x.innerHTML = "Latitude: " + lat + 
-    " Longitude: " + long;
-
-    var http = new XMLHttpRequest();
-
+var http = new XMLHttpRequest();
+    
     http.onreadystatechange = function(){
         if(http.readyState === 4 && http.status === 200){
-            console.log(http.response);
-        }
+            var c = document.getElementById("city");
+            var w = document.getElementById("weather");
+            var d = document.getElementById("degrees");
+            var i = document.getElementById("icon");
+            var f = document.getElementById("farenheit");
+           
+            var json = JSON.parse((http.response));
+            console.log(json);
+            w.innerHTML = json.weather[0].main;
+            c.innerHTML = json.name + ", " + json.sys.country;
+            d.innerHTML = json.main.temp + " " + tempUnit;
+            i.innerHTML = "<img src= ' " + json.weather[0].icon + " ' height='70' width='70' ></img>";
+            var j = document.getElementById("change").onclick = function(){
+                
+                
+            }
+        } 
     }
-
-   
     http.open('GET', "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon="+ long, true);
     http.send();
 
-
 }
+
+function chess() {
+    if(tempUnit == "&#8451") {
+        z = Math.round(json.main.temp) * 9 / 5 + 32;
+        j.innerHTML = z + " " + newTempUnit;
+        console.log("beans");
+    }
+}
+
+
+
+
+
+
+
     
 
