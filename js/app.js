2f3bab688e566dacbd88c8273e3841b52416f090
Jaurs <rhsemail01@gmail.com>
2018-11-27 06:28:48 -0500

    6:30 changes

--- js/app.js
+++ js/app.js
@@ -1,55 +1,42 @@
-var x = document.getElementById("success");
-var tempUnit = "&#8451";
-var newTempUnit = "&#8457";
+$(document).ready(function() {
 
 // Function that will get the location from the user
-function getLocation() {
+
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+        navigator.geolocation.getCurrentPosition(function(position){
+            var lat = position.coords.latitude;
+            var long = position.coords.longitude;
+
+            var api = "https://fcc-weather-api.glitch.me/api/current?lat=" +lat+ '&lon=' +long;
+
+            $.getJSON(api, function(data){
+            var city = data.name;
+            var cel = Math.round(data.main.temp);
+            var far = Math.round(data.main.temp) * 9 / 5 + 32;
+            var desc = data.weather[0].main;
+
+            // Default for th weather is Farenheit
+            $("#temp").html(far + " &#8457");
+
+            // Shows the user the city and description
+            $("#location").html("Location: " + city);
+
+            $("#description").html("Area Description: " + desc)
+
+            $("#fardegree").on("click", function(){
+                $("#temp").html( far + " &#8457");
+            });
+
+            $("#celdegree").on("click", function(){
+                $("#temp").html( cel + " &#8451");
+            });
+
+            console.log(data);
+        });
+    })
 }
-
-// Function that shows the position of the user
-function showPosition(position) {
-
-var lat = position.coords.latitude;
-var long = position.coords.longitude;
-var http = new XMLHttpRequest();
-    
-    http.onreadystatechange = function(){
-        if(http.readyState === 4 && http.status === 200){
-            var c = document.getElementById("city");
-            var w = document.getElementById("weather");
-            var d = document.getElementById("degrees");
-            var i = document.getElementById("icon");
-            var f = document.getElementById("farenheit");
-           
-            var json = JSON.parse((http.response));
-            console.log(json);
-            w.innerHTML = json.weather[0].main;
-            c.innerHTML = json.name + ", " + json.sys.country;
-            d.innerHTML = Math.round(json.main.temp) * 9 / 5 + 32 + " " + newTempUnit;
-            i.innerHTML = "<img src= ' " + json.weather[0].icon + " ' height='70' width='70' ></img>";
-            var j = document.getElementById("change").onclick = function(){
+});
                 
-                
-            }
-        } 
-    }
-    http.open('GET', "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon="+ long, true);
-    http.send();
-
-}
-
-function chess() {
-    if(tempUnit == "&#8451") {
-        z = Math.round(json.main.temp) * 9 / 5 + 32;
-        j.innerHTML = z + " " + newTempUnit;
-        console.log("beans");
-    }
-}
 
 
 
